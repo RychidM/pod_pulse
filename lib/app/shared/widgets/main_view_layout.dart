47b2e5f63317849c24f0784e526c46f65efe9718
RychidM <richardsk2@gmail.com>
2024-08-21 22:40:11 +0000

    responsive layout built

---
+++ lib/app/shared/widgets/main_view_layout.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pod_pulse/app/shared/widgets/mobile_view_layout.dart';
+
+class MainViewLayout extends StatelessWidget {
+  const MainViewLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (context, constrains) {
+          return constrains.maxWidth <= 450 ? const MobileViewLayout() : Stack(
+            children: [
+              Positioned(
+                top: 0,
+                left: 5.w,
+                right: 5.w,
+                bottom: 102.h,
+                child: OrientationBuilder(
+                    builder: (context, orientation) {
+                      return Row(
+                        children: [
+                          AnimatedContainer(
+                            width: constrains.maxWidth <= 1000 || orientation == Orientation.portrait ? 100.w : 350.w,
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple,
+                                borderRadius: BorderRadius.circular(8.r)),
+                            duration: const Duration(milliseconds: 150),
+                          ),
+                          5.0.horizontalSpace,
+                          Expanded(
+                            flex: 10,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.amberAccent,
+                                  borderRadius: BorderRadius.circular(8.r)),
+                            ),
+                          ),
+                          5.horizontalSpace,
+                          AnimatedContainer(
+                            width: 350.w,
+                            decoration: BoxDecoration(
+                                color: Colors.deepOrangeAccent,
+                                borderRadius: BorderRadius.circular(8.r)),
+                            duration: const Duration(milliseconds: 250),
+                          )
+                        ],
+                      );
+                    }
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: 100.h,
+                  width: double.maxFinite,
+                  color: Colors.blue,
+                ),
+              )
+            ],
+          );
+        }
+    );
+  }
+}
